06b38d97b6701466cfa075729af5d43b05d9c0f1
Mathieu Chataigner <mathieu.chataigner@gmail.com>
2013-03-16 14:02:38 +0100

    prout

--- Devoxx_HaskellVousInteresse/build-slides.sh
+++ Devoxx_HaskellVousInteresse/build-slides.sh
@@ -1,1 +1,1 @@
-pandoc -t beamer presentation.mkd -o presentation.pdf -V theme:CambridgeUS --slide-level 2
+pandoc -t beamer presentation.mkd -o presentation.pdf -V theme:CambridgeUS --slide-level 1
